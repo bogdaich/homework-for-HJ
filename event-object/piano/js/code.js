ed5e095572723433b368f7bd1f14a036ea259d32
Bogdan Panchenko <bogdanppppp@gmail.com>
2018-03-13 21:28:40 +0300

    no message

--- event-object/piano/js/code.js
+++ event-object/piano/js/code.js
@@ -3,11 +3,23 @@ let mode = document.getElementsByTagName('ul')[0];
 const audio = document.getElementsByTagName('audio');
 const keys = document.getElementsByTagName('li');
 
-const middleSound = ['sounds/middle/first.mp3', 'sounds/middle/second.mp3', 'sounds/middle/third.mp3', 'sounds/middle/fourth.mp3', 'sounds/middle/fifth.mp3'];
+const middleSound = ['sounds/middle/first.mp3',
+'sounds/middle/second.mp3', '
+sounds/middle/third.mp3',
+'sounds/middle/fourth.mp3',
+'sounds/middle/fifth.mp3'];
 
-const lowerSound = ['sounds/lower/first.mp3', 'sounds/lower/second.mp3', 'sounds/lower/third.mp3', 'sounds/lower/fourth.mp3', 'sounds/lower/fifth.mp3'];
+const lowerSound = ['sounds/lower/first.mp3',
+'sounds/lower/second.mp3',
+'sounds/lower/third.mp3',
+'sounds/lower/fourth.mp3',
+'sounds/lower/fifth.mp3'];
 
-const higherSound = ['sounds/higher/first.mp3', 'sounds/higher/second.mp3', 'sounds/higher/third.mp3', 'sounds/higher/fourth.mp3', 'sounds/higher/fifth.mp3'];
+const higherSound = ['sounds/higher/first.mp3',
+'sounds/higher/second.mp3',
+'sounds/higher/third.mp3',
+'sounds/higher/fourth.mp3',
+'sounds/higher/fifth.mp3'];
 
 let library = {
 	middle: middleSound,
