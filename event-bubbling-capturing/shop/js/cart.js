b5aef471347fdf9a1019d0a05ddfee8f8a727ef8
Bogdan Panchenko <bogdanppppp@gmail.com>
2018-03-28 21:57:52 +0300

    no message

--- event-bubbling-capturing/shop/js/cart.js
+++ event-bubbling-capturing/shop/js/cart.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function cart(event) {
+  let currentItem = event.target;
+  if (currentItem.classList.contains('add-to-cart')) {
+    event.preventDefault();
+    let item = {
+      title: currentItem.getAttribute('data-title'),
+      price: currentItem.getAttribute('data-price')
+    }
+    addToCart(item);
+    event.stopPropagation();
+  }
+}
+let tables = document.getElementsByClassName('items-list');
+tables[0].addEventListener('click', cart);
--- event-bubbling-capturing/table/js/sort.js
+++ event-bubbling-capturing/table/js/sort.js
@@ -1,5 +1,15 @@
 'use strict';
 
 function handleTableClick(event) {
-  
+  let target = event.target;
+  if (target.tagName === 'TH') {
+    if (target.dataset.dir !== undefined) {
+      target.dataset.dir = target.dataset.dir * (-1);
+    }
+    else {
+      target.dataset.dir = 1;
+    };
+    this.dataset.sortBy = target.getAttribute('data-prop-name');
+    sortTable(target.getAttribute('data-prop-name'), target.getAttribute('data-dir') )
+  }
 }
